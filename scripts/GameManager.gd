c01963eecd63558aa05ffc666d389f95a8187ad7
Tofutush <xiexuanqisirocco@163.com>
2025-10-16 11:09:22 -0400

    uuhh

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,11 +1,12 @@
 extends Node
 
+var money = 100
+
 func createDeck() -> Array:
-	var suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
-	var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+	const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+	const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 	var cards = []
 	for rank in ranks:
 		for suit in suits:
 			var card = Card.new(suit, rank)
-			print(card.getValue())
 	return cards
