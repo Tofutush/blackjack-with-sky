751ab2e023026d263b7624262ba0145d7cf9c469
Tofutush <xiexuanqisirocco@163.com>
2025-10-21 23:28:22 -0400

    yay i fixed it

--- scenes/DeckDisplay/deck_display.gd
+++ scenes/DeckDisplay/deck_display.gd
@@ -1,32 +1,26 @@
 extends Control
 class_name DeckDisplay
 
-const OFFSET = 38
-const BEGINNING = 76
-
 var deck: Deck
 
 func _ready() -> void:
 	clear()
 
 func clear() -> void:
-	for child in get_children():
+	for child in $HBoxContainer.get_children():
 		child.queue_free()
 
 func addCard(card: Card, back = false) -> void:
 	var sprite = preload("res://scenes/CardDisplay/card_display.tscn").instantiate()
 	sprite.card = card
-	# offsets it by 38 * the number of children it has already so it shows the side of the prev card
-	print(get_child_count())
-	sprite.position = Vector2(BEGINNING + OFFSET * (get_child_count() - 1), 0)
 	if back: sprite.showBack()
 	else: sprite.showFront()
-	add_child(sprite)
+	$HBoxContainer.add_child(sprite)
 
 func removeCard(idx: int) -> void:
-	get_child(idx).queue_free()
+	$HBoxContainer.get_child(idx).queue_free()
 
 func turnLastBackCard() -> void:
-	if deck and get_child_count() != 0:
-		var child = get_child(get_child_count() - 1)
+	if deck and $HBoxContainer.get_child_count() != 0:
+		var child = $HBoxContainer.get_child($HBoxContainer.get_child_count() - 1)
 		child.showFront()
