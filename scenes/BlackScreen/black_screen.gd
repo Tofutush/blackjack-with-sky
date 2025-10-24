6d70e8a53330943136a6a7581b143a7d9809a4b6
Tofutush <xiexuanqisirocco@163.com>
2025-10-24 16:10:02 -0400

    emit fade finished

--- scenes/BlackScreen/black_screen.gd
+++ scenes/BlackScreen/black_screen.gd
@@ -2,6 +2,8 @@ extends ColorRect
 class_name BlackScreen
 ## FOOLED YA not actually black. you can set color
 
+signal fade_finished ## emit when fade finished, whether in or out
+
 func _ready() -> void:
 	hide()
 
@@ -13,9 +15,13 @@ func setColor(color1: Color) -> void:
 func fadeIn() -> void:
 	show()
 	$AnimationPlayer.play("fade in")
+	await $AnimationPlayer.animation_finished
+	fade_finished.emit()
 
 ## make screen hide in 1s
 func fadeOut() -> void:
 	show()
 	$AnimationPlayer.play("fade out")
+	await $AnimationPlayer.animation_finished
+	fade_finished.emit()
 	hide()
