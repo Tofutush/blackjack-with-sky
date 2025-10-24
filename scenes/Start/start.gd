16e3b0befc85d65131822dec71914ff5d9b6af89
Tofutush <xiexuanqisirocco@163.com>
2025-10-24 16:27:04 -0400

    statr screen fade

--- scenes/Start/start.gd
+++ scenes/Start/start.gd
@@ -3,6 +3,7 @@ extends Control
 
 func _ready() -> void:
 	$Button.grab_focus()
+	$BlackScreen.fadeOut()
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Main/Main.tscn")
