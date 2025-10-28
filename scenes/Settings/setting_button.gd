519b9006228e818a01185abf4290fc3380b7cdc1
Tofutush <xiexuanqisirocco@163.com>
2025-10-28 15:01:06 -0400

    setting toggle

---
+++ scenes/Settings/setting_button.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+@onready var panel_container: PanelContainer = $Control/PanelContainer
+
+var shown := false
+
+func _ready() -> void:
+	panel_container.hide()
+
+func _on_toggled() -> void:
+	if shown:
+		panel_container.hide()
+		shown = false
+	else:
+		panel_container.show()
+		shown = true
+
+func _on_close_button_pressed() -> void:
+	panel_container.hide()
+	shown = false
+
+func _on_exit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Start/Start.tscn")
