e0f0d2b7ef118f9c4eecbadc0150fdeec028b34c
Tofutush <xiexuanqisirocco@163.com>
2025-10-28 15:14:20 -0400

    i give up putting setting in main

--- scenes/Settings/setting_button.gd
+++ scenes/Settings/setting_button.gd
@@ -1,6 +1,6 @@
-extends CanvasLayer
+extends Control
 
-@onready var panel_container: PanelContainer = $Control/PanelContainer
+@onready var panel_container: PanelContainer = $PanelContainer
 
 var shown := false
 
@@ -8,6 +8,7 @@ func _ready() -> void:
 	panel_container.hide()
 
 func _on_toggled() -> void:
+	print('toggled')
 	if shown:
 		panel_container.hide()
 		shown = false
