36363440a56a32b29561e64c0a52d35ad3a0fc15
Tofutush <xiexuanqisirocco@163.com>
2025-10-28 17:59:10 -0400

    start screen buttons

--- scenes/Start/start.gd
+++ scenes/Start/start.gd
@@ -2,10 +2,24 @@ extends Control
 ## literally startscreen
 
 func _ready() -> void:
-	$Button.grab_focus()
 	$BlackScreen.fadeOut()
+	$SettingsContainer.hide()
 
 func _on_button_pressed() -> void:
+	$NewGameWarning.show()
+
+func _on_continue_button_pressed() -> void:
+	$BlackScreen.fadeIn()
+	await $BlackScreen.fade_finished
+	get_tree().change_scene_to_file("res://scenes/Main/Main.tscn")
+
+func _on_settings_button_pressed() -> void:
+	$SettingsContainer.show()
+
+func _on_yes_button_pressed() -> void:
 	$BlackScreen.fadeIn()
 	await $BlackScreen.fade_finished
 	get_tree().change_scene_to_file("res://scenes/Intro/intro.tscn")
+
+func _on_no_button_pressed() -> void:
+	$NewGameWarning.hide()
